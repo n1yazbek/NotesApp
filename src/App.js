1c5212a898635233bb752e11d7b7733c1ed6ef74
Niiazbek Mamasaliev (Nokia) <niiazbek.mamasaliev@nokia.com>
2023-03-15 16:46:15 +0100

    react router step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
-      <NotesListPage/>
+      <Route path='/' exact component={NotesListPage} />
+      <Route path="/note" component={NotePage}/>
     </div>
+    </Router>
   );
 }
 
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import notes from '../assets/data'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
   return (
     <div>
-      Notes
+      <div className='notes-list'>
+        {notes.map((note, index)=>(
+          <ListItem key={index} note ={note}/>
+
+        ))
+        }
+      </div>
     </div>
   )
 }
